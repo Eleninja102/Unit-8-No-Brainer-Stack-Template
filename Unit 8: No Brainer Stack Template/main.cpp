2bbb3afb5c7b21bd32bcbb3bc3fcc98ea2077f64
Coleton Watt <electricninja102@gmail.com>
2021-11-01 08:12:54 -0600

    Initial Commit

---
+++ Unit 8: No Brainer Stack Template/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Unit 8: No Brainer Stack Template
+//
+//  Created by Coleton Watt on 11/1/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
